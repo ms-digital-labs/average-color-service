0aeaf126c963f48ca7e1d387f57154aaf292387e
Tom Lea <commit@tomlea.co.uk>
2014-04-29 17:30:29 +0100

    Handle jsonp.

--- web.go
+++ web.go
@@ -11,63 +11,78 @@ import (
 )
 
 type response struct {
-  Error string
   Color string
+  Error *string
+}
+
+type formatter func(w http.ResponseWriter, r response)
+func jsonFormatter(w http.ResponseWriter, r response) {
+  j, err := json.Marshal(r)
+  if err != nil {
+    j = []byte("{Error:\"Oops\",Color:\"#ffffff\"}")
+  }
+  w.Header().Set("Content-Type", "text/json")
+  w.Write(j)
+}
+
+func jsonpFormatter(callback string) formatter {
+  return func(w http.ResponseWriter, r response) {
+    j, err := json.Marshal(r)
+    if err != nil {
+      j = []byte("{Error:\"Oops\",Color:\"#ffffff\"}")
+    }
+
+    w.Header().Set("Content-Type", "text/jsonp")
+    fmt.Fprintf(w, "%s(%s)", callback, string(j))
+  }
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-Type", "text/json")
+  var format formatter
+  switch x := r.FormValue("callback"); x {
+    case "": format = jsonFormatter
+    default: format = jsonpFormatter(x)
+  }
+
+
   res, err := http.Get(r.FormValue("url"))
   if err != nil {
-    handleError(w, err)
+    handleError(w, err, format)
     return
-  } else {
-    img, _, err := image.Decode(res.Body)
-    if err != nil {
-      handleError(w, err)
-      return
-    } else {
-      bounds := img.Bounds()
-      var count, totalR, totalG, totalB uint64
+  }
 
-      for x := bounds.Min.X; x < bounds.Max.X; x++ {
-        for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
-          r,g,b,_:= img.At(x, y).RGBA()
-          r /= 256
-          g /= 256
-          b /= 256
+  img, _, err := image.Decode(res.Body)
+  if err != nil {
+    handleError(w, err, format)
+    return
+  }
+  bounds := img.Bounds()
+  var count, totalR, totalG, totalB uint64
 
-          if r < 255 || g < 255 || b < 255 {
-            count++
-            totalR += uint64(r)
-            totalG += uint64(g)
-            totalB += uint64(b)
-          }
-        }
+  for x := bounds.Min.X; x < bounds.Max.X; x++ {
+    for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+      r,g,b,_:= img.At(x, y).RGBA()
+      r /= 256
+      g /= 256
+      b /= 256
+
+      if r < 255 || g < 255 || b < 255 {
+        count++
+        totalR += uint64(r)
+        totalG += uint64(g)
+        totalB += uint64(b)
       }
-      myResponse := response{
-        Color: fmt.Sprintf("#%02x%02x%02x",
-          totalR/count,
-          totalG/count,
-          totalB/count,
-        )}
-      j, err := json.Marshal(myResponse)
-      if err != nil {
-        handleError(w, err)
-        return
-      }
-      w.Write(j)
     }
   }
+  color := fmt.Sprintf("#%02x%02x%02x", totalR/count, totalG/count, totalB/count)
+  w.Header().Set("Cache-control", "public, max-age=259200")
+  format(w, response{Color: color})
 }
 
-func handleError(w http.ResponseWriter, err error) {
-  j, err := json.Marshal(response{Error: err.Error()})
-  if err != nil {
-    j = []byte("{Error:\"Oops\"}")
-  }
+func handleError(w http.ResponseWriter, err error, format formatter) {
   w.WriteHeader(500)
-  w.Write(j)
+  v := err.Error()
+  format(w, response{Error: &v, Color: "#ffffff"})
 }
 
 func main() {
